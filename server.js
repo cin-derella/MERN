6e42d913c9f80bfc2b08b945e263fba1892e4bff
cin-derella <shirley_cinderella@outlook.com>
2019-08-01 00:14:33 +0800

    some play

--- server.js
+++ server.js
@@ -2,7 +2,22 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('API Running'));
+app.post('/test.html', (req, res) =>
+  res.send(
+    '<html>' +
+      '<head><title>cin.derel.la</title></head>' +
+      '<h1>Welcome from post</h1>' +
+      '</html>'
+  )
+);
+var j = '';
+app.get('/welcome.html', (req, res) => {
+  for (i = 0; i < 10; i++) {
+    j += `${i}`;
+    console.log(i);
+  }
+  res.send(`${j}`);
+});
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
