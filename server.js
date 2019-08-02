004d70321ea15ae251a22f3e08a2e884ea9e5833
cin-derella <shirley_cinderella@outlook.com>
2019-08-03 00:57:16 +0800

    connect db, use of await and async

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect Database
+
+console.log('begin connectDB');
+connectDB();
+
+console.log('after connectDB');
+
+app.get('/', (req, res) => res.send('API Running'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+// Explaination for post/get
 app.post('/test.html', (req, res) =>
   res.send(
     '<html>' +
@@ -18,6 +33,5 @@ app.get('/welcome.html', (req, res) => {
   }
   res.send(`${j}`);
 });
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+//
