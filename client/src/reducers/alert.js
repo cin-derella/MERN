ce9caf19576640b32f973d15d92048757ddf5b65
cin-derella <shirley_cinderella@outlook.com>
2019-12-04 23:34:42 +0800

    alert components

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -3,11 +3,13 @@ const initialState = [];
 
 export default function(state = initialState, action) {
   const { type, payload } = action;
-
+  console.log(`type/${type}/`);
   switch (type) {
     case SET_ALERT:
+      console.log(`set alert/${payload}/`);
       return [...state, payload];
     case REMOVE_ALERT:
+      console.log(`remove alert/${payload}/`);
       return state.filter(alert => alert.id !== payload);
     default:
       return state;
